487a4c8ab7e1f50a3066eb4fc13de188ad52e6cf
georgef166 <george.farag166@gmail.com>
2025-11-20 23:26:07 -0500

    Added environment variable in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 # This is a Flask application that serves as an API endpoint for querying data from Google BigQuery.
 
 # Import necessary libraries
+import os
 from flask import Flask, jsonify
 from flask_cors import CORS
 from google.cloud import bigquery
@@ -8,6 +9,8 @@ from google.cloud import bigquery
 # Initialize the Flask-CORS app
 app = Flask(__name__)
 CORS(app)
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys/service-account.json"
 
 # Define API endpoint for BigQuery
 @app.route("/api/bq")
